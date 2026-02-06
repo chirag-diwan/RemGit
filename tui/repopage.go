6a583ffd5201c5bf4b58dacc6dba7024691eb4bd
chirag-diwan <diwanashutosh685@gmail.com>
2026-02-06 22:50:52 +0530

    Removed the pre development features before merging

--- tui/repopage.go
+++ tui/repopage.go
@@ -1,19 +1,12 @@
 package tui
 
 import (
-	"bytes"
 	"fmt"
-	"github.com/blacktop/go-termimg"
 	"github.com/charmbracelet/bubbles/viewport"
 	tea "github.com/charmbracelet/bubbletea"
 	"github.com/charmbracelet/glamour"
 	"github.com/charmbracelet/lipgloss"
 	"github.com/chirag-diwan/RemGit/githubapi"
-	"github.com/chirag-diwan/RemGit/markdown"
-	"github.com/disintegration/imaging"
-	"image"
-	"io"
-	"net/http"
 	"strings"
 	"time"
 )
@@ -31,15 +24,6 @@ var (
 
 type ReadmeMsg string
 
-type imageRenderedMsg struct {
-	Path string
-	Data string
-}
-
-type imagePathsMsg []string
-
-type transformedMsg string
-
 type RepoPageModel struct {
 	Width         int
 	Height        int
@@ -53,69 +37,6 @@ type RepoPageModel struct {
 	Imgmap        map[string]string
 	Count         int
 	CacheHeader   string
-}
-
-func transformMarkdownCmd(md string, imgMap map[string]string) tea.Cmd {
-	trans := markdown.TransformMd(md, imgMap)
-	return func() tea.Msg {
-		return transformedMsg(trans)
-	}
-}
-
-func getImagePaths(md string) tea.Cmd {
-	paths := markdown.GetPaths(md)
-	return func() tea.Msg {
-		return imagePathsMsg(paths)
-	}
-}
-
-func downloadAndRenderImage(path, url string) tea.Cmd {
-	return func() tea.Msg {
-		resp, err := http.Get(url)
-		if err != nil {
-			fmt.Print(err)
-		}
-		defer resp.Body.Close()
-		data, err := io.ReadAll(resp.Body)
-		reader := bytes.NewReader(data)
-		imgDec, _, err := image.Decode(reader)
-		if err != nil {
-			fmt.Print(err)
-		}
-		optimizedImg := imaging.Resize(imgDec, 600, 0, imaging.Linear)
-		img := termimg.New(optimizedImg)
-		if err != nil {
-			return imageRenderedMsg{
-				Path: "",
-				Data: "",
-			} // Fallback
-		}
-		renderer := img.Size(imgwidth, imgheight). // Cells (adjust as needed)
-								Scale(termimg.ScaleAuto)
-		switch imgstyle {
-		case "kitty":
-			renderer = renderer.Protocol(termimg.Kitty)
-		case "iterm2":
-			renderer = renderer.Protocol(termimg.ITerm2)
-		case "sixel":
-			renderer = renderer.Protocol(termimg.Sixel)
-		case "halfblocks":
-			renderer = renderer.Protocol(termimg.Halfblocks)
-		default:
-			renderer = renderer.Protocol(termimg.Auto)
-		}
-		rendered, err := renderer.Render()
-		if err != nil {
-			return imageRenderedMsg{
-				Path: "",
-				Data: "",
-			}
-		}
-		return imageRenderedMsg{
-			Path: path,
-			Data: rendered,
-		}
-	}
 }
 
 func NewRepoPageModel(data githubapi.Repository, userdata githubapi.UserSummary, camefrom int, width int, height int) RepoPageModel {
@@ -269,37 +190,11 @@ func (m RepoPageModel) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 
 	case ReadmeMsg:
 		m.LoadingReadme = false
-		m.RawReadme = string(msg)
 		m.ReadmeText, err = glamour.Render(string(msg), "dark")
 		if err != nil {
 			panic(err)
 		}
 		m.Viewport.SetContent(m.renderFullPage())
-		return m, getImagePaths(m.RawReadme)
-	case imagePathsMsg:
-		var cmds []tea.Cmd
-		paths := msg
-		m.Count = len(paths)
-		for _, path := range paths {
-			url := fmt.Sprintf("https://raw.githubusercontent.com/%s/%s/master/%s", m.CurrentRepo.Owner.Login, m.CurrentRepo.Name, path)
-			cmds = append(cmds, downloadAndRenderImage(path, url))
-		}
-		return m, tea.Batch(cmds...)
-
-	case imageRenderedMsg:
-		m.Count--
-		m.Imgmap[msg.Path] = msg.Data
-		if m.Count == 0 {
-			return m, transformMarkdownCmd(m.ReadmeText, m.Imgmap)
-		}
-	case transformedMsg:
-		//	m.ReadmeText, err = glamour.Render(string(msg), "dark")
-		//	if err != nil {
-		//		panic(err)
-		//	}
-		m.ReadmeText = string(msg)
-		m.Viewport.SetContent(m.renderFullPage())
-
 	case tea.WindowSizeMsg:
 		m.Width = msg.Width
 		m.Height = msg.Height
